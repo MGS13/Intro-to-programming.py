864d81fc00240e6d0cae5c0b6352003f2a8b73bc
Mariogs <85800261+MGS13@users.noreply.github.com>
2022-04-16 17:57:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import math
+
+# question1= input("What is your first name?\n")
+# question2= input("what is your last name?\n")
+# name= question1 + " " + question2
+# a_mattress= input("Do we inflate air mattress?")
+
+# print("Hello " + question1 + " " + question2 + "!\n")
+# print("Our suspect is first name: " + question1 + "\nlast name: " + question2)
+# print(a_mattress)
+# if(a_mattress=="y" or "Y"): #AHHHH I DON'T REMEMBER BOOLEAN VALUES
+#   print("Air mattress has been inflated and placed.\n")
+  
+# print(name.capitalize())
+
+# print("Suspect " + name + " is on the roof")
+# print("incident reported at 11 am, 3/21/22")
+# print()
+# print("Suspect has spent",8, "hours on the roof")
+
+
+# x="helo"
+# x=5
+# print("oh" + x + "!")
+
+fname =input("What is your first name? \n")
+lname =input("What is your last name? \n")
+age =input("What is your age? \n")
+eating =input("How long have you been eating? (YEAH A WEIRD QUESTION) \n")
+
+print("Can you believe " + fname + " has been eating for " + eating + " years?!" + " What a " + lname + ". So typical of them at that age of " + age + " minutes." )
